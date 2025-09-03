f1b4886b0693349c1313e4ccb1b595ff17ec0fd7
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-02 22:55:57 -0300

    Update Example 5_2

--- livroJS/Cap05(Repetições)/ex5_2/exemplo5_2.js
+++ livroJS/Cap05(Repetições)/ex5_2/exemplo5_2.js
@@ -4,12 +4,12 @@ const resp = document.querySelector("h3")
 
 // Add um ouvinte de eventos
 frm.addEventListener("submit", (e) => {
-    e.preventDefault() //evita envio de formulário
+    e.preventDefault();
 
-    const numero = Number(frm.inNumero.value) // obtém o número digitado
-    let resposta = `Entre ${numero} e 1: ` //string para mostrar a resposta
-    for (let i = numero; i > 0; i - 1) {
-        resposta = resposta + i + ","
+    const numero = Number(frm.inNumero.value);
+    let resposta = `Entre ${numero} e 1: ${numero}`;
+    for (let i = numero - 1; i > 1; i--) {
+        resposta = resposta + ", " + i;
     }
-    resp.innerText = resposta // Exibe a resposta
-})
+    resp.innerText = resposta;
+});
