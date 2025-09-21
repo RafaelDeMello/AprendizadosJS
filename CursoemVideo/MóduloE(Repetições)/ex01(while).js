83f6381f523f46d55ee98c561801509e76591b0f
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-21 17:14:33 -0300

    Added Módulo E (Repetições) CursoEmVideo

---
+++ CursoemVideo/MóduloE(Repetições)/ex01(while).js
@@ -0,0 +1,11 @@
+/*
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+*/
+var c = 1  //Declara a variavel que vai receber o contador
+while (c <= 3){ // while = enquanto  --> (condição) {bloco de execução de código}
+ console.log(`Passo ${c}`) // Bloco de código a ser executado enquanto condição verdadeira
+ c++
+}
---
+++ CursoemVideo/MóduloE(Repetições)/ex02(do...while).js
@@ -0,0 +1,12 @@
+/*
+var c = 1
+while (c <= 3){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+var c = 1   //declara variavel contador
+do { // Executa o bloco de código ao menos uma vez
+    console.log(`Passo ${c}`) // Bloco de códiugo
+    c++ //Bloco de código
+} while (c <= 3)  // Verifica a condição, caso seja verdadeira volta para a execução do bloco de código
