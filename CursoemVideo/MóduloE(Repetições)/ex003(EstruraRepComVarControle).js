2485ecd2868bc0b25f0de7fe0a448ecc8861b43b
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-21 23:13:39 -0300

    Added Ex003(EstrurasDeRepComVarControle)

---
+++ CursoemVideo/MóduloE(Repetições)/ex003(EstruraRepComVarControle).js
@@ -0,0 +1,14 @@
+/* 
+  var c = 1
+  while (c <= 10) {
+       c++
+  }
+*/
+
+// for(var c = 1; c <= 10; c++){}
+
+// Os dois códigos acima chegam exatamente no mesmo resultado
+
+for (var c = 1; c <= 3; c++){
+    console.log(`Passo ${c}`)
+}
