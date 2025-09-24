6ae81da8714eed9e006b4d236b960d339c54dd4f
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-24 19:35:29 -0300

    tentativa 1 test

---
+++ livroJS/Cap05(Repetições)/Exercicios/test1/index.js
@@ -0,0 +1,20 @@
+function clicar() {
+    const num = document.getElementById("inNumero")
+    numS = Number(num.value)
+    const res = document.getElementById("outRes")
+
+    if(numS == 0){
+        window.alert("Preencha o campo corretamente!")
+    } else {
+        let estrelas = ""
+        for (let i = 1; i <= numS; i++){
+            if (i % 2 == 1) {
+                estrelas += "*"
+            } else {
+              estrelas = estrelas + "-"
+            }
+        }   
+        res.innerText = ""
+    }
+    
+}
