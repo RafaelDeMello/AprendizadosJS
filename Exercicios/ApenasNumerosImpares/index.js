6e3ac097fe3c71409d2462d6ff6c96e6548ace1e
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-27 00:02:04 -0300

    Update

--- Exercicios/ApenasNumerosImpares/index.js
+++ Exercicios/ApenasNumerosImpares/index.js
@@ -1,10 +1,20 @@
-function mostrar(){
-   const res = document.getElementById("res")
-   let impares = ""
-    for(let i = 1; i <= 50; i++){
+function mostrar() {
+  const res = document.getElementById("res");
+  let impares = "";
+  let c = 1;
+  /*for(let i = 1; i <= 50; i++){
         if(i % 2 == 1){
            impares += i + " "
         }
        res.innerText = `Numeros impares = ${impares}` 
     }
+    */
+  while (c <= 50) {
+     if(c % 2 == 1){
+           impares += c + " "
+        }
+    c ++
+  }
+
+  res.innerText = `Números ímpares:${impares}`
 }
