7d2061317619a041438a423867c4e8dfb47e3685
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-27 16:54:28 -0300

    update resolucao do exercicio

--- Exercicios/NumerosInteirosIntervalo/index.js
+++ Exercicios/NumerosInteirosIntervalo/index.js
@@ -2,6 +2,7 @@ function mostrar(){
     const valor1 = document.getElementById("inNumero1")
     const valor2 = document.getElementById("inNumero2")
     const res = document.getElementById("res")
+    const res2 = document.getElementById("res2")
 
     let n1 = Number(valor1.value)
     let n2 = Number(valor2.value)
@@ -22,9 +23,12 @@ function mostrar(){
     }
 
     let resposta = ""
+    let soma = 0
 
     for (let i = inicio ; i <= fim; i++){
         resposta += i + " "
+        soma = soma + i
     }
     res.innerText = `Os números são: ${resposta}.`
+    res2.innerText = `A soma dos números é ${soma}`
 }
