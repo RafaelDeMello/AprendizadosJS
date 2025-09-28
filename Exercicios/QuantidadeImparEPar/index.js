413c2a4a19a896308b51ce609bb41e7d82106760
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-28 01:54:02 -0300

    Added Exercice

--- Exercicios/QuantidadeImparEPar/index.js
+++ Exercicios/QuantidadeImparEPar/index.js
@@ -1,14 +1,20 @@
 function comecar(){
-    const divPergunstas = document.getElementById("perguntas")
-    const num = document.getElementById("txtN")
     const res = document.getElementById("res")
+    const res2 = document.getElementById("res2")
 
-    let n = Number(num.value)
 
+    let par = 0
+    let impar = 0
     for (let i = 1; i <= 10; i++){
-        divPergunstas.style += "display: block;"
-
+          const num = Number(prompt(`Digite o ${i}º valor:`))
+          res.innerText += `${i}º valor: ${num}\n`
+          if(num % 2 ==0){
+            par ++
+          } else {
+            impar ++
+          }
+          res2.innerText = `Total de números pares: ${par} \n
+          Total de números impares: ${impar}`
         }
-
-
+    
 }
