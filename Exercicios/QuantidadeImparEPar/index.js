09e5d78df8203da6eab559d08821d8b26d23e803
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-28 00:47:57 -0300

    Exercicio par ou impar e teste

---
+++ Exercicios/QuantidadeImparEPar/index.js
@@ -0,0 +1,14 @@
+function comecar(){
+    const divPergunstas = document.getElementById("perguntas")
+    const num = document.getElementById("txtN")
+    const res = document.getElementById("res")
+
+    let n = Number(num.value)
+
+    for (let i = 1; i <= 10; i++){
+        divPergunstas.style += "display: block;"
+
+        }
+
+
+}
