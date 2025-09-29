f1e89bf078f04b570a9bf16321388c9a16847bcb
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-09-29 17:16:23 -0300

    Create Slice And Splice.js

---
+++ livroJS/Cap06(Arrays-Vetores)/SliceAndSplice.js
@@ -0,0 +1,10 @@
+const letras = ["A", "B", "C", "D"] //Declara e define conteúdo inicial do vetor
+const letras2 = letras.slice(-2) //Obtém as 2 ultimas letras
+const letras3 = letras.slice(0, -1) //obtem do inicio ate o final, exceto ultima linha
+console.log(letras) // ['A', 'B', 'C', 'D']
+console.log(letras2) // ['C', 'D']
+console.log(letras3) // ['A', 'B', 'C']
+
+const retira = letras.splice(2, 1) //remove a partir da posição 2, 1 elemento
+console.log(letras) // ['A', 'B', 'D']
+console.log(retira) // ['C']
