b603b922e0a6d5b2fd82f48bfca8318a59f14efc
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-10-13 16:08:59 -0300

    update 3 exercise

---
+++ livroJS/Cap06(Arrays-Vetores)/exercicios(inicial)[ ]/ex1.js
@@ -0,0 +1,10 @@
+// 1. Um array esparso é um tipo de array no qual a maioria dos elementos tem valores indefinidos.
+// Crie um array esparso e atribua valores a alguns elementos do array (por exemplo, defina valores nos índices 1, 3 e 7).\
+// Imprima no console o array e também a informação de comprimento do array.
+
+const vetor = []
+vetor[1] = "a";
+vetor[3] = "b";
+vetor[7] = "c";
+console.log(vetor);
+console.log(vetor.length);
---
+++ livroJS/Cap06(Arrays-Vetores)/exercicios(inicial)[ ]/ex2.js
@@ -0,0 +1,6 @@
+// 2. Crie um programa em Node.js que inicie com um array cinco elementos e atribua um novo valor para a primeira posição 
+// (índice 0) desse array. Em seguida, exiba o array antes e depois da alteração.
+const vetor = [ 0 , 2, 3, 5, 6]
+console.log(vetor)
+vetor[0] = 1
+console.log(vetor)
