98c15c348c74d4c1225bca5663cc04c0f4eabba0
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-10-14 23:15:33 -0300

    Update Ex6

--- livroJS/Cap06(Arrays-Vetores)/exercicios(inicial)[ ]/ex6.js
+++ livroJS/Cap06(Arrays-Vetores)/exercicios(inicial)[ ]/ex6.js
@@ -1,7 +1,18 @@
-const vetor=[5, 2]
+const vetor=[ 1 , 2 ]
 
 function MenorOuMaior(n1, n2){
-     return n1 < n2 ?  [n1 , n2] : [n2 , n1]
+     if (n1 > n2 ){
+          return vetor.reverse()
+     } else {
+          return vetor
+     }
+}
+console.log(MenorOuMaior(vetor[0], vetor[1]))
+
+const vetor2 = [5 ,2]
+
+function MenorOuMaior2(n1, n2){
+  return n1 > n2 ? [n2 , n1] : [n1, n2]
 }
 
-console.log(MenorOuMaior(vetor[0], vetor[1]))
+console.log(MenorOuMaior2(vetor2[0], vetor2[1]))
