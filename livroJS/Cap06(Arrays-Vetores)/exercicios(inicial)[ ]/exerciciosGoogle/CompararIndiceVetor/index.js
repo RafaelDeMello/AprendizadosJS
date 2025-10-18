464798e4d8a7f1c43d9bf3016f1192bb01e3808e
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-10-18 11:50:57 -0300

    Update index.js

--- livroJS/Cap06(Arrays-Vetores)/exercicios(inicial)[ ]/exerciciosGoogle/CompararIndiceVetor/index.js
+++ livroJS/Cap06(Arrays-Vetores)/exercicios(inicial)[ ]/exerciciosGoogle/CompararIndiceVetor/index.js
@@ -1,14 +1,29 @@
 const vetor = [1, 2, 3, 1, 2, 3, 1, 1] // Indices que contem o numero um [0 ,3, 6, 7]
 const vetInd = []
 
+// function acharInd(vet, valor){
+//         for(let i = 0; i < vet.length; i++){
+//             if(valor == vetor[i]){
+//                 vetInd.push(i)
+//             }
+//         }
+//         return vetInd
+// }
+
+// console.log(acharInd(vetor, 1))
+
+
+// Com forEach
+
+
 function acharInd(vet, valor){
-        for(let i = 0; i < vet.length; i++){
-            if(valor == vetor[i]){
-                vetInd.push(i)
+        vetor.forEach((ele, ind) => {
+            if(ele == valor){
+                vetInd.push(ind)
             }
-        }
+        });
         return vetInd
 }
-
-console.log(acharInd(vetor, 1))
+acharInd(vetor, 1)
+console.log(vetInd)
 
